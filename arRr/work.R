38522b850a0bf05edcb4ecd27fcff345897c8fe2
Michael Wilson <mikebwilson96@gmail.com>
2020-03-21 14:25:52 -0400

    updates

--- arRr/work.R
+++ arRr/work.R
@@ -1,38 +1,48 @@
-# Create a scatterplot
-plot(x = pirates$height,
-     y = pirates$weight,
-     main = 'My first scatterplot',
-     xlab = 'Height (in cm)',
-     ylab = 'Weight (in kg)',
-     pch = 16,
-     col = gray(.0, .1))
+boat.names <- c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j")
+boat.colors <- c("black", "green", "pink", "blue", "blue", 
+                 "green", "green", "yellow", "black", "black")
 
-grid()
+boat.ages <- c(143, 53, 356, 23, 647, 24, 532, 43, 66, 86)
+boat.prices <- c(53, 87, 54, 66, 264, 32, 532, 58, 99, 132)
+boat.costs <- c(52, 80, 20, 100, 189, 12, 520, 68, 80, 100)
 
-model <- lm(formula = weight ~ height,
-            data = pirates)
 
-abline(model, col = 'blue')
+boat.prices[boat.colors == "green" | boat.colors == "yellow"]
 
-pirateplot(formula = age ~ sword.type,
-           data = pirates,
-           main = "Pirateplot")
+boat.prices[boat.names == "s"] <- 100
 
-t.test(formula = age ~ headband,
-       data = pirates,
-       alternative = 'two.sided')
 
-cor.test(formula = ~ height + weight,
-         data = pirates)
+# What was the median price of black boats less than 100 years old?
+median(boat.prices[boat.colors == "black" & boat.ages < 100])
 
-tat.sword.lm <- lm(formula = tattoos ~ sword.type,
-                   data = pirates)
-anova(tat.sword.lm)
+# How many pink boats were there?
+sum(boat.colors == "pink")
 
-tchests.model <- lm(formula = tchests ~ age + weight + tattoos,
-                    data = pirates)
-summary(tchests.model)
+#What percent of boats were older than 100 years old?
+mean(boat.ages > 100)
 
-# Bayesian version
-ttestBF(formula = age ~ headband,
-        data = pirates)
+# What is the first boat age (3 times)
+rep(boat.ages[1], 3)
+boat.ages[c(1, 1, 1)]
+
+my.index <- 3:5
+boat.ages[my.index]
+
+
+# Which ages are > 100?
+boat.ages > 100
+
+# Which ages are equal to 23?
+boat.ages == 23
+
+# Which boat names are equal to c?
+boat.names == "c"
+
+#Which boats had a higher price than cost?
+boat.prices < boat.costs
+
+# What were the prices of boats older than 100?
+boat.prices[boat.ages > 100]
+
+# Which boats are older than 100 years?
+boat.names[boat.prices > 200 | boat.prices < 100]
