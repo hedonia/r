7eaeb78cdf6bf549f588fb5b0cfc20583e578d46
Michael Wilson <mikebwilson96@gmail.com>
2020-03-22 11:47:48 -0400

    update 3

--- arRr/work.R
+++ arRr/work.R
@@ -1,2 +1,5 @@
 survey <- data.frame("index" = c(1, 2, 3, 4, 5),
                      "age" = c(24, 25, 42, 56, 22))
+
+names(survey)[1] <- "participant.number"
+names(survey)[names(survey) == "age"] <- "years"
